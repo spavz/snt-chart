87e42689cdc3e119c8e530477ee82353725053b6
spavz <vishak1100@gmail.com>
2019-05-06 23:49:02 +0530

    Add python creds

--- cred.py
+++ cred.py
@@ -2,18 +2,11 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
-# Use the application default credentials
-firebase_admin.initialize_app(None, {
-    'apiKey': "AIzaSyARtKwQPQ6h1ZqwOtjb2T6Mfp8CLX8yBb4",
-    'authDomain': "snt-hackathon.firebaseapp.com",
-    'databaseURL': "https://snt-hackathon.firebaseio.com",
-    'projectId': "snt-hackathon",
-    'storageBucket': "snt-hackathon.appspot.com",
-    'messagingSenderId': "574673774825",
-    'appId': "1:574673774825:web:d9aec68343b5932d"
-})
-db = firestore.client()
+# Use a service account
+cred = credentials.Certificate('./model-calling-239818-f2f7f1c21162.json')
+firebase_admin.initialize_app(cred)
 
+db = firestore.client()
 
 
 # write operation 
